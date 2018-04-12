4caefac59cf11efab7951287979e514b689fa76d
amogh <amogh.s.phadke@gmail.com>
2018-04-11 22:25:04 -0600

    init commit

---
+++ src/main/java/com/asp/security/securitydbexample/service/CustomUserDetailsService.java
@@ -0,0 +1,30 @@
+package com.asp.security.securitydbexample.service;
+
+import com.asp.security.securitydbexample.repository.UsersRepository;
+import com.asp.security.securitydbexample.model.CustomUserDetails;
+import com.asp.security.securitydbexample.model.Users;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UsersRepository usersRepository;
+
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Optional<Users> optionalUsers = usersRepository.findByName(username);
+
+        optionalUsers
+                .orElseThrow(() -> new UsernameNotFoundException("Username not found"));
+        return optionalUsers
+                .map(CustomUserDetails::new).get();
+    }
+}
